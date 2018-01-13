83354fa5d79f982858cff2cbf8d9e941e611b909
Johannes Hilden <hildenjohannes@gmail.com>
2018-01-14 02:07:21 +0300

    Implement c functions

--- editline_idris.c
+++ editline_idris.c
@@ -7,6 +7,65 @@
 #include <unistd.h>
 #include "editline_idris.h"
 
+struct entry
+{
+    char         *str;
+    struct entry *next;
+};
+
+static struct entry *dict = NULL;
+
+static char *
+complete (char *token, int *match)
+{
+    struct entry *current = dict;
+    size_t len = strlen (token);
+    char *result = NULL;
+    int count = 0;
+
+    while (NULL != current)
+    {
+        if (0 == strncmp (current->str, token, len))
+        {
+            result = current->str;
+            ++count;
+        }
+        current = current->next;
+    }
+
+    if (1 == count)
+    {
+        *match = 1;
+        return strdup (result + len);
+    }
+
+    return NULL;
+}
+
+static int
+list_possible (char *token, char ***av)
+{
+    struct entry *current = dict;
+    size_t len = strlen (token);
+    char **results = NULL;
+    int count = 0;
+
+    while (NULL != current)
+    {
+        if (0 == strncmp (current->str, token, len))
+        {
+            ++count;
+            results = realloc (results, count * sizeof (char *));
+            *(results + count - 1) = strdup (current->str);
+        }
+        current = current->next;
+    }
+
+    *av = results;
+
+    return count;
+}
+
 char *
 readline_gets (char *prompt)
 {
@@ -18,4 +77,20 @@ readline_gets (char *prompt)
     input = readline (prompt);
 
     return input;
+}
+
+void 
+readline_init ()
+{
+    rl_set_complete_func (&complete);
+    rl_set_list_possib_func (&list_possible);
+}
+
+void
+add_dict_entry (char *str)
+{
+    struct entry *entry = malloc (sizeof (struct entry));
+    entry->str = strdup (str);
+    entry->next = dict;
+    dict = entry;
 }
--- editline_idris.h
+++ editline_idris.h
@@ -2,5 +2,7 @@
 #define EDITLINE_IDRIS_H
 
 char *readline_gets (char *prompt);
+void readline_init ();
+void add_dict_entry (char *str);
 
 #endif
